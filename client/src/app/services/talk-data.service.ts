44ccd8f494941d9702f53d212e5f1d0a75e6e300
Veljko Zrnić <vezr@zuehlke.com>
2018-06-07 17:15:14 +0200

    fixed mocked talks data

--- client/src/app/services/talk-data.service.ts
+++ client/src/app/services/talk-data.service.ts
@@ -10,10 +10,10 @@ export class TalkDataService {
   private allTalks: Array<Talk> = [
     this.mock(1, "First scheduled", true, new Date("2018/03/11 16:30:00"), "description of first", 3),
     this.mock(2, "Second scheduled", true, new Date("2018/05/17 16:30:00"), "description of second", 4),
-    this.mock(3, "First prepared", new Date("2018/04/12 16:30:00"), true, "description of first", 1),
-    this.mock(4, "Second prepared", new Date("2018/02/23 16:30:00"), true, "description of second", 2),
-    this.mock(5, "Third prepared", new Date("2018/05/11 16:30:00"), true, "description of third", 4),
-    this.mock(6, "Fourth prepared", new Date("2018/06/03 16:30:00"), true, "description of fourth", 5)
+    this.mock(3, "First prepared", false, new Date("2018/04/12 16:30:00"), "description of first", 1),
+    this.mock(4, "Second prepared", false, new Date("2018/02/23 16:30:00"), "description of second", 2),
+    this.mock(5, "Third prepared", false, new Date("2018/05/11 16:30:00"), "description of third", 4),
+    this.mock(6, "Fourth prepared", false, new Date("2018/06/03 16:30:00"), "description of fourth", 5)
   ];
 
   constructor() { }
