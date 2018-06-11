799fab17b6bf7c7636d624c21d6be6872caedc29
Veljko Zrnić <vezr@zuehlke.com>
2018-06-11 13:23:57 +0200

    implemented filtering

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Talk } from '../model/talk';
 import { TalkDataService } from '../services/talk-data.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { FormGroup, FormControl } from '@angular/forms';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 
 @Component({
@@ -16,20 +17,46 @@ export class HomeComponent implements OnInit {
   formGroup = new FormGroup({
     filter: new FormControl()
   });
-  
-  scheduledTalks$: Observable<Array<Talk>>;
-  preparationTalks$: Observable<Array<Talk>>;
+
+  scheduledTalks: Array<Talk>;
+  preparationTalks: Array<Talk>;
+  schedTalks$: BehaviorSubject<Array<Talk>>;
+  prepTalks$: BehaviorSubject<Array<Talk>>;
 
   filter: string;
 
   constructor(private talkDataService: TalkDataService) { }
 
   ngOnInit() {
-    this.scheduledTalks$ = this.talkDataService.getScheduledTalks();
-    this.preparationTalks$ = this.talkDataService.getPreparedTalks();
+    this.schedTalks$ = new BehaviorSubject([]);
+    this.prepTalks$ = new BehaviorSubject([]);
+
+    this.talkDataService.getScheduledTalks().subscribe(data => {
+      this.scheduledTalks = data;
+      this.schedTalks$.next(this.filterIt(data));
+    });
+
+    this.talkDataService.getPreparedTalks().subscribe(data => {
+      this.preparationTalks = data;
+      this.prepTalks$.next(this.filterIt(data));
+    });
 
     this.formGroup.valueChanges.debounceTime(500).subscribe((value) => {
       console.log("searching for '" + JSON.stringify(value) + "'...");
-    }); 
+      this.filter = value.filter;
+      this.schedTalks$.next(this.filterIt(this.scheduledTalks));
+      this.prepTalks$.next(this.filterIt(this.preparationTalks));
+    });
+  }
+  
+  private filterIt(data: Array<Talk>) {
+    let activeFilter = this.filter;
+    if (data) {
+      let newData = data.filter((elem) => !activeFilter || elem.title.toLowerCase().indexOf(activeFilter.toLowerCase()) !== -1);
+      return newData;
+    }
+    else {
+      return [];
+    }
   }
 }
